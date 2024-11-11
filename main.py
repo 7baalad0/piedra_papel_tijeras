78f4e25f70ce9ac44d7252088b76e11ef3d37713
Alejandro Balado Vila <alexbaladovila@gmail.com>
2024-11-11 14:58:39 +0100

    Añadidos contadores de los puntos

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
-
+contador_jugador = 0
+contador_maquina = 0
+    
 def eleccion_jugador() -> str:
     opcionjugador = input("Escoge Piedra, Papel o Tijeras: ").lower()
     return opcionjugador
@@ -17,35 +19,41 @@ def eleccion_maquina() -> str:
 
 
 def partida():
+    global contador_jugador, contador_maquina
     jugador = eleccion_jugador()
     maquina = eleccion_maquina()
 
-    print("Jugador:", jugador," Máquina:", maquina) 
+    print("Jugador:", jugador," Máquina:", maquina)  
 
     if jugador == "piedra":
         if maquina == "piedra":
             return "Empate."
         elif maquina == "papel":
+            contador_maquina = contador_maquina + 1
             return "Has perdido..."
         elif maquina == "tijeras":
+            contador_jugador = contador_jugador + 1
             return "¡Tú ganas!"
-
     elif jugador == "papel":
         if maquina == "piedra":
+            contador_jugador = contador_jugador + 1
             return "¡Tú ganas!"
         elif maquina == "papel":
             return "Empate."
         elif maquina == "tijeras":
+            contador_maquina = contador_maquina + 1
             return "Has perdido..."
 
     elif jugador == "tijeras":
         if maquina == "piedra":
+            contador_maquina = contador_maquina + 1
             return "Has perdido..."
         elif maquina == "papel":
+            contador_jugador = contador_jugador + 1
             return "¡Tú ganas!"
         elif maquina == "tijeras":
             return "Empate."
     else:
         return "Escoge una opcion válida."
 
-print(partida())
+print(partida(), "Jugador :", contador_jugador, "Maquina:", contador_maquina)
