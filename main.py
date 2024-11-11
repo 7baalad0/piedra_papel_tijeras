2244e008e939409d384b8583a4ad62e971baa62d
Alejandro Balado Vila <alexbaladovila@gmail.com>
2024-11-11 14:53:05 +0100

    Funciona la elección aleatoria de la máquina

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import random
+
 def eleccion_jugador() -> str:
     opcionjugador = input("Escoge Piedra, Papel o Tijeras: ").lower()
     return opcionjugador
 
-print(eleccion_jugador())
+
+def eleccion_maquina() -> str:
+    random_maquina = random.randint(0,2)
+    if random_maquina == 0:
+        opcion_maquina = "piedra"
+    if random_maquina == 1:
+        opcion_maquina = "papel"
+    if random_maquina == 2:
+        opcion_maquina = "tijeras"
+    return opcion_maquina
+
+print (eleccion_maquina())
