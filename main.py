2ce4c8fac0e1582584a1facd32b229778f0a3af1
Alejandro Balado Vila <alexbaladovila@gmail.com>
2024-11-11 14:54:10 +0100

    Funciona la partida y sus reglas, se puede jugar

--- main.py
+++ main.py
@@ -15,4 +15,37 @@ def eleccion_maquina() -> str:
         opcion_maquina = "tijeras"
     return opcion_maquina
 
-print (eleccion_maquina())
+
+def partida():
+    jugador = eleccion_jugador()
+    maquina = eleccion_maquina()
+
+    print("Jugador:", jugador," Máquina:", maquina) 
+
+    if jugador == "piedra":
+        if maquina == "piedra":
+            return "Empate."
+        elif maquina == "papel":
+            return "Has perdido..."
+        elif maquina == "tijeras":
+            return "¡Tú ganas!"
+
+    elif jugador == "papel":
+        if maquina == "piedra":
+            return "¡Tú ganas!"
+        elif maquina == "papel":
+            return "Empate."
+        elif maquina == "tijeras":
+            return "Has perdido..."
+
+    elif jugador == "tijeras":
+        if maquina == "piedra":
+            return "Has perdido..."
+        elif maquina == "papel":
+            return "¡Tú ganas!"
+        elif maquina == "tijeras":
+            return "Empate."
+    else:
+        return "Escoge una opcion válida."
+
+print(partida())
