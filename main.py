07b1da3c6f8856461e08a3e63699f7baf9d30778
Alejandro <alexbaladovila@gmail.com>
2024-11-13 22:43:50 +0100

    Añadidas frases aleatorias para cuando pierdes o ganas la partida (me parecía divertido añadirlo)

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
 contador_jugador = 0
 contador_maquina = 0
+
+def frase_ganar():
+    frase = ["Esta vez ganas. Pura potra", "Esa no me la esperaba. Tú ganas", "Ganaste. Aunque me estaba dejando, échate una revancha y verás"]
+    return random.choice(frase)
+
+def frase_perder():
+    frase = ["Has perdido. Muy predecible todo.", "Pierdes... ¿De verdad pensabas que no me la venía venir?", "Perdiste. Necesito un rival decente..."]
+    return random.choice(frase)
     
 def eleccion_jugador() -> str:
     return input("Escoge Piedra, Papel o Tijeras: ").lower()
@@ -33,27 +41,27 @@ def partida(opcionjugador , maquina, contador_maquina, contador_jugador):
             resultado = "Empate."
         elif maquina == "papel":
             contador_maquina += 1
-            resultado = "Has perdido..."
+            resultado = "Has perdido la ronda"
         elif maquina == "tijeras":
             contador_jugador += 1
-            resultado = "¡Tú ganas!"
+            resultado = "¡Ganas esta ronda!"
     elif opcionjugador == "papel":
         if maquina == "piedra":
             contador_jugador += 1
-            resultado = "¡Tú ganas!"
+            resultado = "¡Ganas esta ronda!"
         elif maquina == "papel":
             resultado = "Empate."
         elif maquina == "tijeras":
             contador_maquina +=1
-            resultado = "Has perdido..."
+            resultado = "Has perdido la ronda"
 
     elif opcionjugador == "tijeras":
         if maquina == "piedra":
             contador_maquina +=1
-            resultado = "Has perdido..."
+            resultado = "Has perdido la ronda"
         elif maquina == "papel":
             contador_jugador +=1
-            resultado = "¡Tú ganas!"
+            resultado = "¡Ganas esta ronda!"
         elif maquina == "tijeras":
             resultado = "Empate."
     else:
@@ -71,9 +79,9 @@ while jugar == "s":
         print(resultado, "Jugador:", contador_jugador, "Máquina:", contador_maquina)
         
     if contador_jugador == 3:
-        print("¡Que suerte, me has ganado!")
+        print(frase_ganar())
     elif contador_maquina == 3:
-        print("Vaya, has perdido...")
+        print(frase_perder())
     
     jugar_de_nuevo = ""
     while jugar_de_nuevo !="s" and jugar_de_nuevo !="n":
