46a56eccb4ac78edf6579f3e7490ba1ef49732b6
Alejandro <alexbaladovila@gmail.com>
2024-11-13 23:53:39 +0100

    Se añadió un bucle para la pregunta de activar trampas, se añadieron las anotaciones de tipo y se comentó el código

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import random
-contador_jugador = 0
-contador_maquina = 0
+from typing import Union, Tuple
+contador_jugador:int = 0
+contador_maquina:int = 0
+maquina_tramposa:bool = False
 
-def frase_ganar():
-    if contador_jugador == 3 and contador_maquina == 0:  
+def frase_ganar() -> str:  #Frases aleatorias para cuando ganas la partida
+    if contador_jugador == 3 and contador_maquina == 0:   #Frase especial para el 3 - 0
         return "Menuda paliza histórica me acabas de meter. Mi enhorabuena"
     frase = ["Esta vez ganas. Pura potra", "Esa no me la esperaba. Tú ganas", "Ganaste. Aunque me estaba dejando, échate una revancha y verás"]
     return random.choice(frase)
 
-def frase_perder():
-    if contador_maquina == 3 and contador_jugador == 0: 
+def frase_perder() -> str:  #Frases aleatorias para cuando pierdes la partida
+    if contador_maquina == 3 and contador_jugador == 0: #Frase especial para el 0 - 3
         return "Vaya, perder así es incluso mas díficil que ganar. Siéntete orgulloso"
     frase = ["Has perdido. Muy predecible todo.", "Pierdes... ¿De verdad pensabas que no me la venía venir?", "Perdiste. Necesito un rival decente..."]
     return random.choice(frase)
     
-def eleccion_jugador() -> str:
+def eleccion_jugador() -> str:   #Elección del jugador
     return input("Escoge Piedra, Papel o Tijeras: ").lower()
     
-def eleccion_maquina(opcionjugador) -> str:
+def eleccion_maquina(opcionjugador) -> Union [str,int]:  #Elección de la máquina
     
-    if maquina_tramposa and contador_jugador > contador_maquina:
+    if maquina_tramposa and contador_jugador > contador_maquina:  #Si la maquina tramposa fue activada y se cumplen las condiciones hará trampas
         if opcionjugador == "piedra":
             return "papel"  
         elif opcionjugador == "papel":
@@ -27,8 +29,8 @@ def eleccion_maquina(opcionjugador) -> str:
         elif opcionjugador == "tijeras":
             return "piedra"  
     
-    else:
-        random_maquina = random.randint(0,2)
+    else:                                          #En cualquier otro caso escogerá una opción aleatoria
+        random_maquina = random.randint(0,2)  
         if random_maquina == 0:
             return "piedra"
         elif random_maquina == 1:
@@ -36,11 +38,11 @@ def eleccion_maquina(opcionjugador) -> str:
         elif random_maquina == 2:
             return "tijeras"
 
-def partida(opcionjugador , maquina, contador_maquina, contador_jugador):
+def partida(opcionjugador:str, maquina:Union[str,int], contador_maquina:int, contador_jugador:int) -> Tuple [int,int,str]: #Funcion que define las reglas de la partida y quién gana cada ronda
  
-    print("Jugador:", opcionjugador ," Máquina:", maquina) 
+    print("Jugador:", opcionjugador ," Máquina:", maquina)  #Enseña que sacó el jugador y la máquina en la ronda
 
-    if opcionjugador == "piedra":
+    if opcionjugador == "piedra": 
         if maquina == "piedra":
             resultado = "Empate."
         elif maquina == "papel":
@@ -73,31 +75,36 @@ def partida(opcionjugador , maquina, contador_maquina, contador_jugador):
         
     return contador_maquina, contador_jugador, resultado
 
-jugar = "s"
-while jugar == "s": 
+jugar:str = "s"     
+while jugar == "s":  #Bucle principal del juego, llama al resto de funciones y hace que las rondas mientras no se llegue a 3 y las partidas vuelvan a empezar mientras quieras
     
-    activar_trampa = input("¿Quieres activar la máquina tramposa? (s/n): ").lower()
-    if activar_trampa == "s":
-        maquina_tramposa = True
-        print("Máquina tramposa activada.")
-    else:
-        maquina_tramposa = False
-        print("Se ha desactivado la máquina tramposa.")
+    activar_trampa:str = ""
+    while activar_trampa !="s" and activar_trampa !="n":      #Bucle para activar o desactivar la máquina tramposa
+        activar_trampa = input("¿Quieres activar la máquina tramposa? (s/n): ").lower()
+        if activar_trampa == "s":
+            maquina_tramposa = True
+            print("Máquina tramposa activada.")
+            break
+        elif activar_trampa == "n":
+            print("Se ha desactivado la máquina tramposa.")
+            break
+        else:
+            print("Introduce un valor correcto")
 
-    while contador_jugador < 3 and contador_maquina < 3:
+    while contador_jugador < 3 and contador_maquina < 3:    #Bucle que hace que se jueguen rondas hasta que alguien llegue a 3 puntos
         opcionjugador = eleccion_jugador()
         maquina = eleccion_maquina(opcionjugador )
         contador_maquina, contador_jugador, resultado = partida(opcionjugador , maquina, contador_maquina, contador_jugador)
         print(resultado, "Jugador:", contador_jugador, "Máquina:", contador_maquina)
         
-    if contador_jugador == 3:
+    if contador_jugador == 3:     #Finaliza la partida diciendo el ganador con las frases aleatorias
         print(frase_ganar())
     elif contador_maquina == 3:
         print(frase_perder())
     
-    jugar_de_nuevo = ""
-    while jugar_de_nuevo !="s" and jugar_de_nuevo !="n":
-        jugar_de_nuevo = input("¿Quieres la revancha? (s/n): ").lower()
+    jugar_de_nuevo = ""                                                  
+    while jugar_de_nuevo !="s" and jugar_de_nuevo !="n":                    #Pregunta al final de cada partida si se quiere volver a jugar. Si no, el programa finalizará.
+        jugar_de_nuevo = input("¿Quieres la revancha? ;) (s/n): ").lower()
         if jugar_de_nuevo == "s":
             contador_jugador = 0
             contador_maquina = 0
