fccd4869c44db2570e0ffa01664a7ace50b3447f
Alejandro <alexbaladovila@gmail.com>
2024-11-13 20:48:43 +0100

    Restructurado el código para que sea mas limpio y correcto. Se ha añadido la variable "resultado" y se ha eliminado la funcion "jugar" sustituyendose por un bucle, para no llamar a la funcion dentro de la funcion

--- main.py
+++ main.py
@@ -18,69 +18,71 @@ def eleccion_maquina() -> str:
     return opcion_maquina
 
 
-def partida():
-    global contador_jugador, contador_maquina
-    jugador = eleccion_jugador()
-    maquina = eleccion_maquina()
+def partida(opcionjugador, maquina, contador_maquina, contador_jugador):
+ 
+    print("Jugador:", opcionjugador," Máquina:", maquina) 
 
-    print("Jugador:", jugador," Máquina:", maquina) 
-
-    if jugador == "piedra":
+    if opcionjugador == "piedra":
         if maquina == "piedra":
-            return "Empate."
+            resultado = "Empate."
         elif maquina == "papel":
             contador_maquina = contador_maquina + 1
-            return "Has perdido..."
+            resultado = "Has perdido..."
         elif maquina == "tijeras":
             contador_jugador = contador_jugador + 1
-            return "¡Tú ganas!"
-    elif jugador == "papel":
+            resultado = "¡Tú ganas!"
+    elif opcionjugador == "papel":
         if maquina == "piedra":
             contador_jugador = contador_jugador + 1
-            return "¡Tú ganas!"
+            resultado = "¡Tú ganas!"
         elif maquina == "papel":
-            return "Empate."
+            resultado = "Empate."
         elif maquina == "tijeras":
             contador_maquina = contador_maquina + 1
-            return "Has perdido..."
+            resultado = "Has perdido..."
 
-    elif jugador == "tijeras":
+    elif opcionjugador == "tijeras":
         if maquina == "piedra":
             contador_maquina = contador_maquina + 1
-            return "Has perdido..."
+            resultado = "Has perdido..."
         elif maquina == "papel":
             contador_jugador = contador_jugador + 1
-            return "¡Tú ganas!"
+            resultado = "¡Tú ganas!"
         elif maquina == "tijeras":
-            return "Empate."
+            resultado = "Empate."
     else:
-        return "Escoge una opcion válida."
+        resultado = "Escoge una opcion válida."
+        
+    return contador_maquina, contador_jugador, resultado
 
-def jugar():
-    global contador_jugador, contador_maquina
+jugar = "s"
+while jugar == "s": 
 
     while contador_jugador < 3 and contador_maquina < 3:
-        print(partida(), "Jugador:", contador_jugador, "Máquina:", contador_maquina)
-
+        opcionjugador = eleccion_jugador()
+        maquina = eleccion_maquina()
+        contador_maquina, contador_jugador, resultado = partida(opcionjugador, maquina, contador_maquina, contador_jugador)
+        print(resultado, "Jugador:", contador_jugador, "Máquina:", contador_maquina)
+        
     if contador_jugador == 3:
-        print("¡Felicidades! Has ganado el juego.")
+        print("¡Que suerte, me has ganado!")
     elif contador_maquina == 3:
-        print("La máquina ha ganado el juego.")
+        print("Vaya, has perdido...")
     
     
    
     jugar_de_nuevo = ""
     while jugar_de_nuevo !="s" and jugar_de_nuevo !="n":
-        jugar_de_nuevo = input("¿Quieres jugar de nuevo? (s/n): ").lower()
+        jugar_de_nuevo = input("¿Quieres la revancha? (s/n): ").lower()
         if jugar_de_nuevo == "s":
             contador_jugador = 0
             contador_maquina = 0
-            jugar()
+            print ("Así me gusta. ¡Vamos allá!")
+            break
         elif jugar_de_nuevo == "n":
             print("Bien jugado, ¡hasta la próxima!")
+            jugar = "n"
+            break
         else:
             print("No te he entendido. Introduce un valor correcto")
             
-
-
-jugar()
